ef6b1c605760998927f11b68b8d86246d43cc42b
Rushikesh Auti <autirushikesh758@gmail.com>
2026-06-07 15:00:41 +0530

    Add home detail functionality

--- models/home.js
+++ models/home.js
@@ -34,4 +34,12 @@ module.exports = class Home {
       callback(!err ? JSON.parse(data) : []);
     });
   };
+  static findById(homeId, callback) {
+    this.fetchAll(homes => {
+      const homeFound = homes.find(home => home.id === homeId);
+      callback(homeFound);
+    })
+  }
 };
+
+ 
