3857151e0bc03dba91a870a23357ad9f541ce551
Rushikesh Auti <autirushikesh758@gmail.com>
2026-06-20 16:08:07 +0530

    Migrate authentication from cookies to sessions

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const path = require("path");
 const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 // Local Module
 const storeRouter = require("./routes/storeRouter");
@@ -20,9 +22,22 @@ app.use(express.static(path.join(rootDir, "public")));
 app.set("view engine", "ejs");
 app.set("views", "views");
 
+const store = new MongoDBStore({
+  uri: process.env.DB_PATH,
+  collection: 'sessions'
+});
+
 app.use(express.urlencoded({ extended: true }));
+
+app.use(session({
+  secret: "StayFinder",
+  resave: false,
+  saveUninitialized: true,
+  store
+}));
+
 app.use((req, res, next) => {
-  req.isLoggedIn = req.get('Cookie') ? req.get('Cookie').split('=')[1] === 'true' : false;
+  req.isLoggedIn = req.session.isLoggedIn;
   next();
 });
 
--- controllers/authController.js
+++ controllers/authController.js
@@ -8,11 +8,13 @@ exports.getLogin = (req, res, next) => {
 
 exports.postLogin = (req, res, next) => {
   console.log(req.body);
-  res.cookie("isLoggedIn", true);
+  req.session.isLoggedIn = true;
+  // res.cookie("isLoggedIn", true);
   res.redirect("/");
 };
 
 exports.postLogout = (req, res, next) => {
-  res.cookie("isLoggedIn", false);
-  res.redirect("/login");
+  req.session.destroy(() => {
+    res.redirect("/login");
+  });
 };
