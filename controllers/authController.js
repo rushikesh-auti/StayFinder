b34b362a2ac8684e47381d6bfcb8de166c2f8653
Rushikesh Auti <autirushikesh758@gmail.com>
2026-06-21 16:32:16 +0530

    Add signup page and routes

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,6 +6,21 @@ exports.getLogin = (req, res, next) => {
   });
 };
 
+exports.getSignup = (req, res, next) => {
+  res.render("auth/signup", {
+    pageTitle: "Signup",
+    currentPage: "Signup",
+    isLoggedIn: false
+  });
+};
+
+exports.postSignup = (req, res, next) => {
+  console.log(req.body);
+  // req.session.isLoggedIn = true;
+  // res.cookie("isLoggedIn", true);
+  res.redirect("/login");
+};
+
 exports.postLogin = (req, res, next) => {
   console.log(req.body);
   req.session.isLoggedIn = true;
