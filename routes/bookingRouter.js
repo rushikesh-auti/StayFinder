2b2ebc463686874d3c7afec6790795aa81bc27dd
Rushikesh Auti <autirushikesh758@gmail.com>
2026-07-15 12:26:43 +0530

    Add booking availability API route

--- routes/bookingRouter.js
+++ routes/bookingRouter.js
@@ -3,6 +3,7 @@ const bookingController = require("../controllers/bookingController");
 const router = express.Router();
 
 router.get("/", bookingController.getBookings);
+router.get("/check-availability/:homeId", bookingController.checkAvailability);
 router.post("/create/:homeId", bookingController.postBooking);
 router.post("/cancel/:bookingId", bookingController.cancelBooking);
 
