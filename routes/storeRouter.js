66c3306f7afdd7b9b8b5f772e8b221c5d61bc035
Rushikesh Auti <autirushikesh758@gmail.com>
2026-07-22 23:43:42 +0530

    Add property search route

--- routes/storeRouter.js
+++ routes/storeRouter.js
@@ -1,14 +1,14 @@
-// External Module
 const express = require("express");
 const storeRouter = express.Router();
 
-// Local Module
 const storeController = require("../controllers/storeController");
 
 storeRouter.get("/", storeController.getIndex);
 storeRouter.get("/homes", storeController.getHomes);
-storeRouter.get("/favourites", storeController.getFavouriteList);
+storeRouter.get("/homes/search", storeController.searchHomes); 
 storeRouter.get("/homes/:homeId", storeController.getHomeDetails);
+
+storeRouter.get("/favourites", storeController.getFavouriteList);
 storeRouter.post("/favourites", storeController.postAddToFavourite);
 storeRouter.post("/favourites/delete/:homeId", storeController.postRemoveFromFavourite);
 
