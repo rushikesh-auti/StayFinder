ab6d64795bf8eedd3541ad5e05dd1051f4c3e92c
Rushikesh Auti <autirushikesh758@gmail.com>
2026-07-25 22:34:38 +0530

    Add booked dates endpoint

--- routes/bookingRouter.js
+++ routes/bookingRouter.js
@@ -6,5 +6,6 @@ router.get("/", bookingController.getBookings);
 router.get("/check-availability/:homeId", bookingController.checkAvailability);
 router.post("/create/:homeId", bookingController.postBooking);
 router.post("/cancel/:bookingId", bookingController.cancelBooking);
+router.get("/:homeId/booked-dates", bookingController.getBookedDates);
 
 module.exports = router;
