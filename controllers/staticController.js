19e64a39b28b63cd83406e80cab56178956459c2
Rushikesh Auti <autirushikesh758@gmail.com>
2026-07-26 17:51:00 +0530

    Fix static page navigation rendering

--- controllers/staticController.js
+++ controllers/staticController.js
@@ -1,6 +1,8 @@
 exports.getPrivacy = (req, res) => {
   res.render("pages/privacy", {
     pageTitle: "Privacy Policy",
+    currentPage: "privacy",
+    isLoggedIn: req.isLoggedIn,
     user: req.session.user || null,
   });
 };
@@ -8,13 +10,17 @@ exports.getPrivacy = (req, res) => {
 exports.getTerms = (req, res) => {
   res.render("pages/terms", {
     pageTitle: "Terms & Conditions",
+    currentPage: "terms",
+    isLoggedIn: req.isLoggedIn,
     user: req.session.user || null,
   });
 };
 
 exports.getContact = (req, res) => {
   res.render("pages/contact", {
-    pageTitle: "Contact Us",
+    pageTitle: "Contact",
+    currentPage: "contact",
+    isLoggedIn: req.isLoggedIn,
     user: req.session.user || null,
   });
 };
