50217881044a39ee32d03b9c61fb8c72a01972f2
Rushikesh Auti <autirushikesh758@gmail.com>
2026-08-01 15:44:48 +0530

    Configure global flash middleware for toast messages

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ require("dotenv").config();
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 const multer = require('multer');
+const flash = require("connect-flash");
 
 // Local Module
 const storeRouter = require("./routes/storeRouter");
@@ -67,6 +68,13 @@ app.use((req, res, next) => {
     req.session.redirectTo = req.originalUrl;
   }
   req.isLoggedIn = req.session.isLoggedIn;
+  next();
+});
+
+app.use(flash());
+app.use((req, res, next) => {
+  res.locals.successMessage = req.flash("success");
+  res.locals.errorMessage = req.flash("error");
   next();
 });
 
