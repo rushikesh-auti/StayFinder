7aecde74717fb2a1a3235760626ed8dd6b7b36f8
Rushikesh Auti <autirushikesh758@gmail.com>
2026-08-05 15:49:08 +0530

    Add user profile route

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -3,10 +3,18 @@ const authRouter = express.Router();
 
 const authController = require("../controllers/authController.js");
 
+const isAuth = (req, res, next) => {
+  if (req.session.isLoggedIn) {
+    return next();
+  }
+  return res.redirect("/login");
+};
+
 authRouter.get("/login", authController.getLogin);
 authRouter.post("/login", authController.postLogin);
 authRouter.post("/logout", authController.postLogout);
 authRouter.get("/signup", authController.getSignup);
 authRouter.post("/signup", authController.postSignup);
+authRouter.get("/profile", isAuth, authController.getProfile);
 
 module.exports = authRouter;
